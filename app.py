8bd5694bba2df279f090ab1ff097eaca5ee09d56
devaminb <106449125+devaminb@users.noreply.github.com>
2022-10-25 11:25:39 +0200

    Add support for CORS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 from flask import Flask
+from flask_cors import CORS
 
 uploadDirectoryAbsolutePath = Path('upload').absolute()
 UPLOAD_FOLDER = uploadDirectoryAbsolutePath
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
